be10e92b3d206458296b769c6f0a8acb313bfa8b
Juni2799 <akshaankhanna@gmail.com>
2024-04-29 22:17:00 +0530

    Added Publisher Entity, Repository and Boot Data

--- src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring6webapp.bootstrap;
 
 import guru.springframework.spring6webapp.models.Author;
 import guru.springframework.spring6webapp.models.Book;
+import guru.springframework.spring6webapp.models.Publisher;
 import guru.springframework.spring6webapp.repositories.AuthorRepository;
 import guru.springframework.spring6webapp.repositories.BookRepository;
+import guru.springframework.spring6webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +14,12 @@ public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -28,8 +32,12 @@ public class BootstrapData implements CommandLineRunner {
         ddd.setTitle("Domain Driven Design");
         ddd.setIsbn("123456");
 
+        Publisher uniPub = new Publisher();
+        uniPub.setPublisherName("Universal Publications Ltd.");
+
         Author ericSaved = authorRepository.save(eric);
         Book dddSaved = bookRepository.save(ddd);
+        Publisher uniPubSaved = publisherRepository.save(uniPub);
 
         Author rod = new Author();
         rod.setFirstName("Rod");
@@ -39,19 +47,34 @@ public class BootstrapData implements CommandLineRunner {
         noEJB.setTitle("J2EEE Development without EJB");
         noEJB.setIsbn("567890");
 
+        Publisher ecoPub = new Publisher();
+        ecoPub.setPublisherName("Economic Publications Ltd.");
+
         Author rodSaved = authorRepository.save(rod);
         Book noEJBSaved = bookRepository.save(noEJB);
+        Publisher ecoPubSaved = publisherRepository.save(ecoPub);
 
         //Setting association between author and books
         ericSaved.getBooks().add(dddSaved);
         rodSaved.getBooks().add(noEJBSaved);
 
+        uniPubSaved.getBooks().add(dddSaved);
+        uniPubSaved.getBooks().add(noEJBSaved);
+        dddSaved.setPublisher(uniPubSaved);
+        noEJBSaved.setPublisher(uniPubSaved);
+
         //Persisting the data as well
         authorRepository.save(ericSaved);
         authorRepository.save(rodSaved);
 
+        publisherRepository.save(uniPubSaved);
+
+        bookRepository.save(dddSaved);
+        bookRepository.save(noEJBSaved);
+
         System.out.println("In Bootstrap...");
         System.out.println("Author Count: " + authorRepository.count());
         System.out.println("Book Count: " + bookRepository.count());
+        System.out.println("Publisher Count: " + publisherRepository.count());
     }
 }
---
+++ src/main/java/guru/springframework/spring6webapp/models/Publisher.java
@@ -0,0 +1,48 @@
+package guru.springframework.spring6webapp.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+public class Publisher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String publisherName, address, city, state;
+    private Long zip;
+
+    @OneToMany(mappedBy = "publisher")
+    private Set<Book> books = new HashSet<>();
+
+    @Override
+    public String toString() {
+        return "Publisher{" +
+                "id=" + id +
+                ", publisherName='" + publisherName + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip=" + zip +
+                ", books=" + books +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Publisher publisher)) return false;
+
+        return getId() != null ? getId().equals(publisher.getId()) : publisher.getId() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getId() != null ? getId().hashCode() : 0;
+    }
+}
